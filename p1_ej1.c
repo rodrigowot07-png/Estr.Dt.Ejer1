a46e8304a14ff86852f3538ff5230e0fd590600e
nicoencinas <nicolas.encinas.giordana@gmail.com>
2026-02-08 15:41:18 +0100

    update p1_ej1

--- p1_ej1.c
+++ p1_ej1.c
@@ -18,16 +18,79 @@
 int main () {
     Music *m1=NULL;
     Music *m2=NULL;
+    Music *m3 = NULL;
+    int cmp;
 
-    m1 = music_initFromString("Id:title:\"Imagine\" artist:\"John Lennon\"");
-
-    m2 = music_initFromString("Id: ");
+    m1 = music_init();
+    m2 = music_init();
 
     if (!m1 || !m2) {
-        fprintf(stderr, "Error creating musics\n");
-        return NULL;
+        printf("Error creating music\n");
+        return 1;
     }
 
+    /*First song*/
     music_setId(m1, 10);
+    music_setTitle(m1, "Bohemian Rhapsody");
+    music_setArtist(m1, "Queen");
+    music_setDuration(m1, 355);
 
+    /*Second song*/
+    music_setId(m2, 20);
+    music_setTitle(m2, "Blinding Lights");
+    music_setArtist(m2, "The Weekend");
+    music_setDuration(m2, 200);
+
+    /*Print both songs*/
+    music_formatted_print(stdout, m1);
+    music_formatted_printf(stdout, m2);
+    printf("\n");
+
+    /*Compare both songs*/
+    cmp = music_cmp(m1, m2);
+
+    if (cmp < 0) {
+        printf("First song is shorter than the second one\n");
+    } else if (cmp > 0) {
+        printf("First song is longer than the third one\n");
+    } else {
+        printf("Both songs have the same duration\n")
+    }
+
+    /*Print second song title*/
+    printf("Second song title: %s\n", music_getTitle(m2));
+
+    /*Copy the first song in a third one*/
+    if (!(m3 = music_copy(m1))) {
+        printf("Error copying music\n");
+        music_free(m1);
+        music_free(m2);
+        return 1;
+    }
+
+    /*Print id of the third song*/
+    printf("Id of the third song: %ld\n", music_getId(m3));
+
+    /*Play the first and the third song*/
+    music_formatted_print(stdout, m1);
+    music_formatted_print(stdout, m3);
+    printf("\n");
+
+    /*Compare the first and the third song*/
+    cmp = music_cmp(m1, m3);
+
+    if (cmp < 0) {
+        printf("First song is shorter than the third one\n");
+    } else if (cmp > 0) {
+        printf("First song is longer than the third one\n");
+    } else {
+        printf("Both songs have the same duration\n");
+    }
+
+    /*Free memory*/
+    music_free(m1);
+    music_free(m2);
+    music_free(m3);
+
+    return 0;
 }
