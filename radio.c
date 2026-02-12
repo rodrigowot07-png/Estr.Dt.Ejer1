d40946515b890d31d97859c6650e573a7a8354e5
rodrigowot07-png <rodrigowot07@gmail.com>
2026-02-12 10:57:36 +0100

    updte

--- radio.c
+++ radio.c
@@ -3,6 +3,7 @@
 
 
 #include "radio.h"
+#include "music.h"
 
 struct _Radio {
     Music *songs[MAX_MSC];
@@ -14,7 +15,7 @@ struct _Radio {
 Radio * radio_init() {
     Radio *r = NULL;
 
-    if (!(r = (Radio *) malloc (sizeof(Radio)))) {
+    if (!(r = (Radio *) calloc (1, sizeof(Radio)))) {
         return  NULL;
     }
 
@@ -30,13 +31,69 @@ void radio_free(Radio *r) {
 }
 
 Status radio_newMusic(Radio *r, char *desc) {
-    
+    long i, j;
+
+    if (!r || !desc) {
+        return ERROR;
+    }
+
+    if (!(r->songs = music_InitFromString(desc))) {
+        return ERROR;
+    }
+
+    for (i = 0; i < r->num_music; i++) {
+        for (j = 0; j < i; j++) {
+            if (r->songs[i].id == r->songs[j].id) {
+                return OK;
+            } else {
+                r->songs[r->num_music] = r->songs[i];
+                r->num_music++;
+            }
+        }
+    }
+
+    return OK;
 }
 
+Status radio_newRelation(Radio *r, long orig, long dest) {
+    long i, j;
 
+    if (!r) {
+        return ERROR;
+    }
 
+    for(i=0; i<=(*r->num_music); i++){
+        if(music_getId(r->songs[i]) == orig){
+            break;
+        }
+        if(i == (*r->num_music)){
+            return ERROR;
+        }
+    }
 
+    for(i=0; i<=(*r->num_music); i++){
+        if(music_getId(r->songs[i]) == dest){
+            break;
+        }
+        if(i == (*r->num_music)){
+            return ERROR;
+        }
+    }
 
+    r->relations[orig][dest] = TRUE;
+    r->relations[dest][orig] = TRUE;
+    r->num_relations++;
+
+    return OK;
+}
+
+Bool radio_contains(const Radio *r, long id) {
+    if (!r) {
+        return FALSE;
+    }
+
+    
+}
 
 int radio_getNumberOfMusic(const Radio *r){
     if(!r){
@@ -46,4 +103,22 @@ int radio_getNumberOfMusic(const Radio *r){
     return r->num_music;
 }
 
-int radio_getNumberOfRelations(const Radio *r);
+int radio_getNumberOfRelations(const Radio *r){
+    if(!r){
+        return -1;
+    }
+    
+    return r->num_relations;
+}
+
+Bool radio_relationExists(const Radio *r, long orig, long dest){
+    long i,j;
+
+    if(!r || music){
+        return FALSE
+    }
+
+    if(r->relations[orig][dest]==TRUE){
+        return TRUE;
+    }
+}
