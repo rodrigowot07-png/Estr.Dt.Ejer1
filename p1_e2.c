07683b123b1c8c970874ebf568a7108ae9631b44
rodrigowot07 <rodrigowot07@gmail.com>
2026-02-18 12:08:13 +0100

    update

--- p1_e2.c
+++ p1_e2.c
@@ -27,16 +27,42 @@ int main() {
     }
 
     stat = radio_newMusic(rad, "id:\"111\" title:\"Paint It, Black\" artist:\"The Rolling Stones\" duration:\"202\" state:\"0\"");
-    fprintf(stdout, "Inserting: Paint It, Black... result...: %d\n", stat == OK ? 1 : 0);
+    fprintf(stdout, "Inserting: Paint It, Black... result...: ");
+    if(stat == OK){
+        fprintf(stdout, "1\n");
+    }
+    else{
+        fprintf(stdout, "0\n");
+    }
 
     stat = radio_newMusic(rad, "id:\"222\" title:\"Every Breath You Take\" artist:\"The Police\" duration:\"253\" state:\"0\"");
-    fprintf(stdout, "Inserting: Every Breath You Take... result...: %d\n\n", stat == OK ? 1 : 0);
+    fprintf(stdout, "Inserting: Every Breath You Take... result...: ");
+    if(stat == OK){
+        fprintf(stdout, "1\n\n");
+    }
+    else{
+        fprintf(stdout, "0\n\n");
+    }
+
 
     stat = radio_newRelation(rad, 222, 111);
     fprintf(stdout, "Inserting radio recommendation: 222 --> 111\n\n");
 
-    fprintf(stdout, "111 --> 222? %s\n", radio_relationExists(rad, 111, 222) ? "Yes" : "No");
-    fprintf(stdout, "222 --> 111? %s\n", radio_relationExists(rad, 222, 111) ? "Yes" : "No");
+    fprintf(stdout, "111 --> 222? ");
+    if(radio_relationExists(rad, 111, 222) == TRUE){
+        fprintf(stdout, "Yes\n");
+    }
+    else{
+        fprintf(stdout, "No\n");
+    }
+
+    fprintf(stdout, "222 --> 111? ");
+    if(radio_relationExists(rad, 222, 111) == TRUE){
+        fprintf(stdout, "Yes\n");
+    }
+    else{
+        fprintf(stdout, "No\n");
+    }
 
     /*Prints the connections*/
     fprintf(stdout, "Number of connections from 111: %d\n", radio_getNumberOfRelationsFromId(rad, 111));
