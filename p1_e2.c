657e0708a2429e067cdd25b1702ec34806230f61
nicoencinas <nicolas.encinas.giordana@gmail.com>
2026-02-18 09:46:24 +0100

    asf

--- p1_e2.c
+++ p1_e2.c
@@ -27,21 +27,21 @@ int main() {
     }
 
     stat = radio_newMusic(rad, "id:\"111\" title:\"Paint It, Black\" artist:\"The Rolling Stones\" duration:\"202\" state:\"0\"");
-    printf("Inserting: Paint It, Black... result...: %d\n", stat == OK ? 1 : 0);
+    fprintf(stdout, "Inserting: Paint It, Black... result...: %d\n", stat == OK ? 1 : 0);
 
     stat = radio_newMusic(rad, "id:\"222\" title:\"Every Breath You Take\" artist:\"The Police\" duration:\"253\" state:\"0\"");
-    printf("Inserting: Every Breath You Take... result...: %d\n\n", stat == OK ? 1 : 0);
+    fprintf(stdout, "Inserting: Every Breath You Take... result...: %d\n\n", stat == OK ? 1 : 0);
 
     stat = radio_newRelation(rad, 222, 111);
-    printf("Inserting radio recommendation: 222 --> 111\n\n");
+    fprintf(stdout, "Inserting radio recommendation: 222 --> 111\n\n");
 
-    printf("111 --> 222? %s\n", radio_relationExists(rad, 111, 222) ? "Yes" : "No");
-    printf("222 --> 111? %s\n", radio_relationExists(rad, 222, 111) ? "Yes" : "No");
+    fprintf(stdout, "111 --> 222? %s\n", radio_relationExists(rad, 111, 222) ? "Yes" : "No");
+    fprintf(stdout, "222 --> 111? %s\n", radio_relationExists(rad, 222, 111) ? "Yes" : "No");
 
-    printf("Number of connections from 111: %d\n", radio_getNumberOfRelationsFromId(rad, 111));
-    printf("Number of connections from 222: %d\n", radio_getNumberOfRelationsFromId(rad, 222));
+    fprintf(stdout, "Number of connections from 111: %d\n", radio_getNumberOfRelationsFromId(rad, 111));
+    fprintf(stdout, "Number of connections from 222: %d\n", radio_getNumberOfRelationsFromId(rad, 222));
 
-    printf("All radio reccomendations:\n");
+    fprintf(stdout, "All radio reccomendations:\n");
     radio_print(stdout, rad);
 
     return 0;
