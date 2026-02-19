561bb3eec7594647be08e55c1aaf09785e959e63
rodrigowot07 <rodrigowot07@gmail.com>
2026-02-19 09:33:17 +0100

    e3

---
+++ p1_e3.c
@@ -0,0 +1,11 @@
+/**
+ * @file  p1_ej3
+ * @author Nicolas Encinas y Rodrigo Martin
+ * @date February 2026
+ * @mersion 1.0
+ * @brief Reeds the information of the file and prints the radio
+ * @details 
+ * 
+ * @see
+ */
+
